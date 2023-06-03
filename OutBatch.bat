d86451808892052cdef34bb0ea174e46c2ff8954
lllmtklll <52519402+lllmtklll@users.noreply.github.com>
2023-06-03 10:21:25 +0900

    Update OutBatch.bat

--- OutBatch.bat
+++ OutBatch.bat
@@ -1,24 +1,38 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM フォルダのパスを指定
 set "folderPath=test"
-set "outputFile=output.txt"
-type nul > "%outputFile%"
 
+REM 出力ファイル名とパスを指定
+set "outputFile=output.csv"
+
+REM テキストファイルを処理してCSVファイルに出力
+echo "File Name","First Number","Second Number" > "%outputFile%"
 for %%F in ("%folderPath%\*.txt") do (
+    REM ファイル名を取得
     set "fileName=%%~nxF"
-    set "matches="
-    set "count=0"
+
+    REM ファイルの内容を読み込んで処理
+    set "firstNumber="
+    set "secondNumber="
     for /f "usebackq delims=" %%L in ("%%F") do (
-        echo %%L | findstr /r "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]" > nul && (
-            set "match=%%L"
-            if defined matches (set "matches=!matches!,!match!") else set "matches=!match!"
-            set /a "count+=1"
-            if !count! equ 2 (
-                echo !fileName!: !matches! >> "%outputFile%"
-                set "matches=" & set "count=0"
+        REM 正規表現を使用して8連続の数字を抽出
+        echo %%L | findstr /r "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]" > nul
+        if not errorlevel 1 (
+            REM 抽出した数字を変数に格納
+            if not defined firstNumber (
+                set "firstNumber=%%L"
+            ) else (
+                set "secondNumber=%%L"
             )
         )
     )
-    if defined matches echo !fileName!: !matches! >> "%outputFile%"
+    REM 1つ目の数字と2つ目の数字をCSVファイルに出力
+    if defined firstNumber (
+        echo "!fileName!","!firstNumber!","!secondNumber!" >> "%outputFile%"
+    )
 )
+
+REM CSVファイルをExcelで開く
+start excel "%outputFile%"
