bfed77a5d3a3af9d26f6360211b2dd4015589981
lllmtklll <52519402+lllmtklll@users.noreply.github.com>
2023-06-05 23:50:30 +0900

    Update OutBatch.bat

--- OutBatch.bat
+++ OutBatch.bat
@@ -21,23 +21,32 @@ for %%F in ("%folderPath%\*.txt") do (
     REM ファイルの内容を読み込んで処理
     set "matches="
     set "count=0"
+    set "foundString="
     for /f "usebackq delims=" %%L in ("%%F") do (
-        REM 正規表現を使用して8連続の数字を抽出
-        echo %%L | findstr /r "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]" > nul
-        if not errorlevel 1 (
-            REM 抽出した数字を変数に追加
-            set "match=%%L"
-            if defined matches (
-                set "matches=!matches!","!match!"
-            ) else (
-                set "matches=!match!"
+        REM 指定した文字列が見つかるまでの間、正規表現を使用して8連続の数字を抽出しない
+        if not defined foundString (
+            echo %%L | findstr /r "ここに文字列を指定" > nul
+            if not errorlevel 1 (
+                set "foundString=1"
             )
-            set /a "count+=1"
-            REM 8つの数字が2つある場合に出力ファイルに追記してリセット
-            if !count! equ 2 (
-                echo "!fileName!","!matches!" >> "%outputFile%"
-                set "matches="
-                set "count=0"
+        ) else (
+            REM 正規表現を使用して8連続の数字を抽出
+            echo %%L | findstr /r "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]" > nul
+            if not errorlevel 1 (
+                REM 抽出した数字を変数に追加
+                set "match=%%L"
+                if defined matches (
+                    set "matches=!matches!","!match!"
+                ) else (
+                    set "matches=!match!"
+                )
+                set /a "count+=1"
+                REM 8つの数字が2つある場合に出力ファイルに追記してリセット
+                if !count! equ 2 (
+                    echo "!fileName!","!matches!" >> "%outputFile%"
+                    set "matches="
+                    set "count=0"
+                )
             )
         )
     )
